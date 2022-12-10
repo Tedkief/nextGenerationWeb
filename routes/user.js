46b6f78bbadb3d0a0bd1808e678896eb8779f2a7
tedkief <teddy.kieffer@epitech.eu>
2022-12-10 13:38:48 +0100

    [ADD] route change the query

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const QueryEngine = require('@comunica/query-sparql').QueryEngine;
 const myEngine = new QueryEngine();
 
-var queryFR = `
+var queryFr = `
   PREFIX dbo: <http://dbpedia.org/ontology/>
   PREFIX dbc: <http://dbpedia.org/resource/Category:>
   PREFIX dct: <http://purl.org/dc/terms/>
@@ -29,7 +29,7 @@ var queryGoat = `
   } LIMIT 20
 `;
 
-var queryFrGoat = `
+var queryCowFr = `
   PREFIX dbo: <http://dbpedia.org/ontology/>
   PREFIX dbc: <http://dbpedia.org/resource/Category:>
   PREFIX dct: <http://purl.org/dc/terms/>
@@ -43,10 +43,21 @@ var queryFrGoat = `
   } LIMIT 20
 `;
 
-router.get('/', async function (req, res, next) {
-  const bindingsStream = await myEngine.queryBindings(queryFrGoat, {
-    sources: ['http://fragments.dbpedia.org/2015/en'],
-  });
+router.get('/:querytype', async function (req, res, next) {
+  console.log(req.params.querytype)
+  if (req.params.querytype == "cowfrenchlover") {
+    bindingsStream = await myEngine.queryBindings(queryCowFr, {
+      sources: ['http://fragments.dbpedia.org/2015/en'],
+    });
+  } else if (req.params.querytype == "frenchlover") {
+    bindingsStream = await myEngine.queryBindings(queryFr, {
+      sources: ['http://fragments.dbpedia.org/2015/en'],
+    });
+  } else {
+    bindingsStream = await myEngine.queryBindings(queryGoat, {
+      sources: ['http://fragments.dbpedia.org/2015/en'],
+    });
+  }
   const bindings = await bindingsStream.toArray();
   res.send(bindings.flatMap(
     value => value.get("name").value))
